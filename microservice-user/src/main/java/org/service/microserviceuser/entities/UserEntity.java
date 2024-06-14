db2ae1ab334257d72a07198150d2c178480ead29
JUAN PABLO RESTREPO JIMENEZ <jprestrepo346@gmail.com>
2024-06-14 09:26:34 -0500

    added dependencies and changed names for config files

--- microservice-user/src/main/java/org/service/microserviceuser/entities/UserEntity.java
+++ microservice-user/src/main/java/org/service/microserviceuser/entities/UserEntity.java
@@ -1,10 +1,13 @@
 package org.service.microserviceuser.entities;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
@@ -17,9 +20,21 @@ public class UserEntity {
 
     @MongoId(targetType = FieldType.OBJECT_ID)
     private String id;
+    @NotBlank
+    @Field(name = "name")
     private String name;
+    @NotBlank
+    @Field(name = "lastname")
     private String lastname;
+    @NotBlank
+    @Field(name = "email")
     private String email;
+    @NotBlank
+    @Field(name = "docId")
+    @Indexed(unique = true)
     private String doc;
+    @NotBlank
+    @Field(name = "phone")
+    private String phone;
 
 }
