6b299b9883a729c6a7acc6c681b18dbe6468a4c7
JUAN PABLO RESTREPO JIMENEZ <jprestrepo346@gmail.com>
2024-06-16 20:58:29 -0500

    finishing microservice-booking

--- microservice-booking/src/main/java/org/service/microservicebooking/clients/BookClient.java
+++ microservice-booking/src/main/java/org/service/microservicebooking/clients/BookClient.java
@@ -4,9 +4,7 @@ import org.service.microservicebooking.DTOs.BookDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 
 @FeignClient(name = "microservice-book",url="http://localhost:8091")
 public interface BookClient {
--- microservice-booking/src/main/java/org/service/microservicebooking/repositories/BookingRepository.java
+++ microservice-booking/src/main/java/org/service/microservicebooking/repositories/BookingRepository.java
@@ -1,7 +1,5 @@
 package org.service.microservicebooking.repositories;
 
-import org.service.microservicebooking.DTOs.BookDTO;
-import org.service.microservicebooking.DTOs.UserDTO;
 import org.service.microservicebooking.entities.BookingEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
